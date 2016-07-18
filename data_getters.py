fff134199d23280b451581ffb077afa252c9ee60
Patrick-Fenerty <Patrick.Fenerty@geocent.com>
2016-07-18 13:18:45 -0500

    Major Cleanup

---
+++ data_getters.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 03 12:48:48 2016
+
+@author: pfenerty
+"""
+
+import pandas as pd
+import json
+import urllib2
+import os.path
+
+
+# region Utils
+
+def j2p(url, index):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    try:
+        response = opener.open(url)
+    except Exception:
+        print('Could not get Stats')
+        return None
+    else:
+        data = json.loads(response.read())
+        headers = data['resultSets'][index]['headers']
+        rows = data['resultSets'][index]['rowSet']
+        data_dict = [dict(zip(headers, row)) for row in rows]
+        return pd.DataFrame(data_dict)
+
+
+def getJson(url, index):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    response = opener.open(url)
+    data = json.loads(response.read())
+    data = data['resultSets'][index]['rowSet']
+    return data
+
+
+def j2p2(url, index):
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    response = opener.open(url)
+    data = json.loads(response.read())
+    headers = data['resultSets'][index]['headers']
+    rows = data['resultSets'][index]['rowSet']
+    data_dict = [dict(zip(headers, row)) for row in rows]
+    return pd.DataFrame(data_dict)
+
+
+def get_year_string(year):
+    return str(year) + "-" + str(year + 1)[2:4]
+
+
+# endregion
+
+# region Enums
+
+# perModes = ["Totals", "PerPossession", "PerPlay", "PerMinute"]
+perModes = ["Totals", "PerPossession"]
+# seasonTypes = ["Pre+Season", "Regular+Season", "Playoffs", "All+Star"]
+seasonTypes = ["Regular+Season", "Playoffs"]
+# measureTypes = ["Base", "Advanced", "Misc", "Scoring", "Usage", "Opponent", "Defense"]
+measureTypes = ["Base", "Advanced"]
+years = range(1996, 2015)
+
+
+# endregion
+
+# region Base Stats
+
+def get_stat_csv(team_or_player, measure_type, per_mode, season_year, season_type):
+    if not os.path.exists('../data/' + team_or_player + '_data/'):
+        os.makedirs('../data/' + team_or_player + '_data')
+        print('MAKING DIRECTORY : ' + '../data/' + team_or_player + '_data')
+    file_path = '../data/' + team_or_player + '_data/' + season_year + '_' + season_type + '_' + measure_type + '_' + per_mode + '.csv'
+    if not os.path.isfile(file_path):
+        print('GET DATA FROM WEB API')
+        url = "http://stats.nba.com/stats/leaguedash{teamOrPlayer}stats?College=&Conference=&Country=&DateFrom=" \
+              "&DateTo=&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=&LastNGames=0" \
+              "&LeagueID=00&Location=&MeasureType={measureType}&Month=0&OpponentTeamID=0&Outcome=&PORound=0" \
+              "&PaceAdjust=N&PerMode={perMode}&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N" \
+              "&Rank=N&Season={seasonYear}&SeasonSegment=&SeasonType={seasonType}&ShotClockRange=&StarterBench=" \
+              "&TeamID=0&VsConference=&VsDivision=&Weight="
+        url = url.format(perMode=per_mode, seasonYear=season_year, seasonType=season_type, measureType=measure_type,
+                         teamOrPlayer=team_or_player)
+        df = j2p(url, 0)
+        if df is not None:
+            df.to_csv(file_path)
+            print('GOT DATA FROM WEB API, WRITING TO FILE')
+            return df
+        else:
+            print('COULD NOT GET DATA FROM WEB API')
+    else:
+        print('GETTING DATA FROM FILE')
+        return pd.read_csv(file_path)
+
+
+def get_all_player_stats():
+    for year in years:
+        year_string = get_year_string(year)
+        for measure_type in measureTypes:
+            for season_type in seasonTypes:
+                for per_mode in perModes:
+                    print(year_string + ": " + measure_type + ", " + season_type + ", " + per_mode)
+                    get_stat_csv("player", measure_type, per_mode, year_string, season_type)
+
+
+# endregion
+
+# region Shot Location Stats
+
+
+def get_shot_location_stats(player_or_team, measure_type, per_mode, season_year, season_type):
+    url = "http://stats.nba.com/stats/leaguedash{playerOrTeam}shotlocations?College=&Conference=&Country=&DateFrom=" \
+          "&DateTo=&DistanceRange=5ft+Range&Division=&DraftPick=&DraftYear=&GameScope=&GameSegment=&Height=" \
+          "&LastNGames=0&LeagueID=00&Location=&MeasureType={measureType}&Month=0&OpponentTeamID=0&Outcome=&PORound=0" \
+          "&PaceAdjust=N&PerMode={perMode}&Period=0&PlayerExperience=&PlayerPosition=&PlusMinus=N&Rank=N" \
+          "&Season={seasonYear}&SeasonSegment=&SeasonType={seasonType}&ShotClockRange=&StarterBench=&TeamID=0" \
+          "&VsConference=&VsDivision=&Weight="
+    url = url.format(playerOrTeam=player_or_team, measureType=measure_type, perMode=per_mode, seasonYear=season_year,
+                     seasonType=season_type)
+    df = j2p(url, 0)
+    if df is not None:
+        df.to_csv("data\\" + player_or_team + "\\" + measure_type + "\\" + season_type + "\\" + per_mode + "\\" +
+                  season_year + ".csv")
+
+
+def get_all_player_shot_location_stats():
+    for year in years:
+        year_string = get_year_string(year)
+        for measure_type in measureTypes:
+            for season_type in seasonTypes:
+                for per_mode in perModes:
+                    print(year_string + ": " + measure_type + ", " + season_type + ", " + per_mode)
+                    get_shot_location_stats("player", measure_type, per_mode, year_string, season_type)
+
+
+# endregion
+
+# region SportsVU
+
+sports_vu_types = ["CatchShoot", "Defense", "Drives", "Passing", "Possessions", "Rebounding", "PullUpShot",
+                   "ElbowTouch", "PostTouch", "PaintTouch"]
+
+
+def get_sports_vu_stats(player_or_team, vu_type, per_mode, season_year, season_type):
+    url = "http://stats.nba.com/stats/leaguedashptstats?College=&Conference=&Country=&DateFrom=&DateTo=" \
+          "&Division=&DraftPick=&DraftYear=&GameScope=&Height=&LastNGames=0&LeagueID=00&Location=&Month=0" \
+          "&OpponentTeamID=0&Outcome=&PORound=0&PerMode={perMode}&PlayerExperience=&PlayerOrTeam={playerOrTeam}" \
+          "&PlayerPosition=&PtMeasureType={vuType}&Season={seasonYear}&SeasonSegment=&SeasonType={seasonType}" \
+          "&StarterBench=&TeamID=0&VsConference=&VsDivision=&Weight="
+    url = url.format(playerOrTeam=player_or_team, vuType=vu_type, seasonYear=season_year, seasonType=season_type,
+                     perMode=per_mode)
+    df = j2p(url, 0)
+    if df is not None:
+        df.to_csv(
+            "data\\" + player_or_team + "\\" + vu_type + "\\" + season_type + "\\" + per_mode + "\\" + season_year)
+
+
+def get_all_sports_vu_stats():
+    for year in range(2013, 2015):
+        year_string = get_year_string(year)
+        for vu_type in sports_vu_types:
+            for season_type in seasonTypes:
+                for per_mode in perModes:
+                    print(year_string + ": " + vu_type + ", " + season_type + ", " + per_mode)
+                    get_sports_vu_stats("player", vu_type, per_mode, year_string, season_type)
+
+
+# endregion
+
+# region Game Logs
+
+def get_player_game_logs(season_year, season_type, player_name, player_id):
+    if not os.path.exists('../data/player_data/'):
+        os.makedirs('../data/player_data')
+        print('MAKING DIRECTORY : ' + '../data/player_data')
+    if not os.path.exists('../data/player_data/game_logs'):
+        os.makedirs('../data/player_data/game_logs')
+        print('MAKING DIRECTORY : ' + '../data/player_data/game_logs')
+    file_path = '../data/player_data/game_logs/' + season_year + '_' + season_type + '_' + player_name + '.csv'
+    if not os.path.isfile(file_path):
+        print("GETTING " + player_name + "'s Game Logs for the " + season_year + " " + season_type + " FROM WEB API!")
+        url = "http://stats.nba.com/stats/playergamelog?" \
+              "LeagueID=00&" \
+              "PlayerID={pid}&" \
+              "Season={seasonYear}&" \
+              "SeasonType={seasonType}"
+        url = url.format(pid=player_id, seasonYear=season_year, seasonType=season_type)
+        df = j2p(url, 0)
+        df.to_csv(file_path)
+        return df
+    else:
+        print("ALREADY HAVE " + player_name + "'s Game Logs for the " + season_year + " " + season_type + " ON FILE!")
+        return pd.read_csv(file_path)
+
+
+# endregion
